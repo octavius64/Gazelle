f942893c6bd6b4556a30e914c13969de5231a8f2
Foo Bar <foo@bar.com>
2023-02-22 01:33:59 +0000

    Fix some issues with permissions, a while loop, missing README, etc.

--- docker/db_initializer_docker_entrypoint.sh
+++ docker/db_initializer_docker_entrypoint.sh
@@ -5,8 +5,8 @@ echo 'Trying to connect to the DB to see if it is online'
 echo 'use sys;' | mysql -u root -p --host="$MYSQL_HOST" --password="$MYSQL_ROOT_PASSWORD"
 while [ $? -ne 0 ]; do
     echo 'MySQL not online yet, waiting for it...'
+    sleep 3
     echo 'use sys;' | mysql -u root -p --host="$MYSQL_HOST" --password="$MYSQL_ROOT_PASSWORD"
-    sleep 5
 done
 
 set -eo pipefail
@@ -17,7 +17,7 @@ NUM_TABLES=`echo 'use gazelle; show tables;' | mysql -u root -p --host="$MYSQL_H
 echo Found $NUM_TABLES tables in gazelle
 
 if [ "$NUM_TABLES" == "0" ]; then
-    echo 'DB is empty, populating it now'...
+    echo 'DB is empty, populating it now...'
     echo 'drop database gazelle;' | mysql -u root -p --host="$MYSQL_HOST" --password="$MYSQL_ROOT_PASSWORD"
     cat gazelle.sql | mysql -u root -p --host="$MYSQL_HOST" --password="$MYSQL_ROOT_PASSWORD"
 fi
@@ -29,7 +29,7 @@ set +o pipefail
 
 echo 'Setting up the named pipe to signal that the DB is ready'
 mkfifo /db_initializer_pipe/db_ready
-chmod 777 /db_initializer_pipe/db_ready
+chmod 644 /db_initializer_pipe/db_ready
 while true
 do
     yes > /db_initializer_pipe/db_ready
