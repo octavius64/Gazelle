029cce3b91734a88a11fb9f9343f51978c6c5b03
Foo Bar <foo@bar.com>
2023-02-26 19:19:51 -0800

    Fix a couple broken links

--- www/design/publicfooter.php
+++ www/design/publicfooter.php
@@ -2,7 +2,7 @@
 	</tr>
 </table>
 <div id="foot">
-	<span><a href="#"><?=SITE_NAME?></a> | <a href="https://what.cd/gazelle/">Project Gazelle</a></span>
+	<span><a href="#"><?=SITE_NAME?></a> | <a href="https://github.com/octavius64/Gazelle">Project Gazelle - Octavius</a></span>
 </div>
 </body>
 </html>
