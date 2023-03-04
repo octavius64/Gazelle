21ecb5736c8aeebbb800738218e842d0c0c8c1ea
Foo Bar <foo@bar.com>
2023-03-04 15:16:03 -0800

    Fix the way nginx entrypoint script generates gazelle config

--- docker/nginx_docker_entrypoint.sh
+++ docker/nginx_docker_entrypoint.sh
@@ -3,34 +3,28 @@
 set -eo pipefail
 
 echo 'Generating config.php from config.template...'
-cat /home/config.template | \
-    sed "s/<GAZELLE_ENCKEY>/"$GAZELLE_ENCKEY"/g" | \
-    sed "s/<GAZELLE_REPORT_PASSWORD>/"$GAZELLE_REPORT_PASSWORD"/g" | \
-    sed "s/<GAZELLE_RSS_KEY>/"$GAZELLE_RSS_KEY"/g" | \
-    sed "s/<GAZELLE_SCHEDULE_KEY>/"$GAZELLE_SCHEDULE_KEY"/g" | \
-    sed "s/<GAZELLE_SITE_PASSWORD>/"$GAZELLE_SITE_PASSWORD"/g" | \
-    sed "s/<GAZELLE_SITE_SALT>/"$GAZELLE_SITE_SALT"/g" | \
-    sed "s/<MYSQL_PASSWORD>/"$MYSQL_PASSWORD"/g" | \
-    sed "s/<GAZELLE_SITE_HOST>/"$GAZELLE_SITE_HOST"/g" | \
-    sed "s/<OCELOT_SITE_HOST>/"$OCELOT_SITE_HOST"/g" > \
-    /gazelle-config-php/config.php
 
-mv /gazelle-config-php/config.php /home/config.template
+cp /home/config.template /gazelle-config-php/config.php
+
+sed -i "s/<GAZELLE_ENCKEY>/"$GAZELLE_ENCKEY"/g" /gazelle-config-php/config.php
+sed -i "s/<GAZELLE_REPORT_PASSWORD>/"$GAZELLE_REPORT_PASSWORD"/g" /gazelle-config-php/config.php
+sed -i "s/<GAZELLE_RSS_KEY>/"$GAZELLE_RSS_KEY"/g" /gazelle-config-php/config.php
+sed -i "s/<GAZELLE_SCHEDULE_KEY>/"$GAZELLE_SCHEDULE_KEY"/g" /gazelle-config-php/config.php
+sed -i "s/<GAZELLE_SITE_PASSWORD>/"$GAZELLE_SITE_PASSWORD"/g" /gazelle-config-php/config.php
+sed -i "s/<GAZELLE_SITE_SALT>/"$GAZELLE_SITE_SALT"/g" /gazelle-config-php/config.php
+sed -i "s/<MYSQL_PASSWORD>/"$MYSQL_PASSWORD"/g" /gazelle-config-php/config.php
+sed -i "s/<GAZELLE_SITE_HOST>/"$GAZELLE_SITE_HOST"/g" /gazelle-config-php/config.php
+sed -i "s/<OCELOT_SITE_HOST>/"$OCELOT_SITE_HOST"/g" /gazelle-config-php/config.php
+
 if [ "$GAZELLE_DEBUG" == "1" ]; then
-    cat /home/config.template | \
-        sed "s/<DEBUG_WARNINGS>/true/g" | \
-        sed "s/<DEBUG_MODE>/true/g" | \
-        sed "s|<DEBUG_LOG_FILE_PATH>|/gazelle_log_pipe/log|g" > \
-        /gazelle-config-php/config.php
+    sed -i "s/<DEBUG_WARNINGS>/true/g" /gazelle-config-php/config.php
+    sed -i "s/<DEBUG_MODE>/true/g" /gazelle-config-php/config.php
+    sed -i "s|<DEBUG_LOG_FILE_PATH>|/gazelle_log_pipe/log|g" /gazelle-config-php/config.php
 else
-    cat /home/config.template | \
-        sed "s/<DEBUG_WARNINGS>/false/g" | \
-        sed "s/<DEBUG_MODE>/false/g" | \
-        sed "s|<DEBUG_LOG_FILE_PATH>|/dev/null|g" > \
-        /gazelle-config-php/config.php
+    sed -i "s/<DEBUG_WARNINGS>/false/g" /gazelle-config-php/config.php
+    sed -i "s/<DEBUG_MODE>/false/g" /gazelle-config-php/config.php
+    sed -i "s|<DEBUG_LOG_FILE_PATH>|/dev/null|g" /gazelle-config-php/config.php
 fi
-
-rm /home/config.template
 
 echo 'Updating nginx conf...'
 sed -i "s/<GAZELLE_SITE_HOST>/"$GAZELLE_SITE_HOST"/g" /etc/nginx/conf.d/default.conf
